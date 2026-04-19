07bfe447d8a4a228cecf3865ae43c04b0c9d8a1a
Renier Lawrence Tuico <renierlawrencetuico143@gmail.com>
2026-04-19 11:05:49 +0800

    Change profile pic and fix animation delay on programming languages

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,10 @@
-import profilePic from "./assets/profile-pic.jpg"
+import profilePic from "./assets/port-prof.png"
 
 function Hero(){
     return(
         <>
             <section className="flex flex-col md:flex-row p-3 lg:w-11/12 mb-3 h-screen place-self-center md:text-start text-center md:justify-center" id="hero">
-                <img src={profilePic} alt="A really handsome photo of Renier Lawrence Tuico" className="rounded-full self-center mb-2 md:mb-0 h-3/6" data-aos="fade-up"/>
+                <img src={profilePic} alt="A really handsome photo of Renier Lawrence Tuico" className="rounded-full self-center mb-2 md:mb-0 h-2/6" data-aos="fade-up"/>
                 <section className="flex flex-col self-stretch justify-center md:ml-4">
                     <p className="mb-4" data-aos="fade-up" data-aos-delay="200">Welcome! I'm Renier Lawrence Tuico, a Software Developer. I enjoy building creative projects, learning new tools and technologies, and sharing what I learn along the way. Take a look at some of my work below!</p>
                     <button className="md:self-start self-center button-85" data-aos="fade-up" data-aos-delay="300" onClick={() => document.getElementById('projects').scrollIntoView({ behavior: 'smooth' })}>View my Projects</button>
